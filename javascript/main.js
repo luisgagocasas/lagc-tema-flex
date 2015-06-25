f03b55f90fa615f54c36d1d91b74df6e0e5617d0
Luis Gago Casas <luisgago@lagc-peru.com>
2015-06-25 10:59:38 -0500

    colores y wrapper 90%

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){ //cuando el DOM listo
+	$("#entrar").click(function(){
+		$("#login").show();
+	});
+	$("#entrar").toggle(
+        function(e){ //Primer click
+            $("#login").show();
+        },
+        function(e){ //Segundo click
+            $("#login").hide();
+        }
+    );
 });

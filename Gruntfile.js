a5e5bc91da44840de62f50189f0fb3a51ae9f727
Luis Gago Casas <luisgago@lagc-peru.com>
2015-07-24 18:22:36 -0500

    blog,cv terminado

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,136 +1,143 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        //Observar cambios
-        watch: {
-            options: {
-                nospawn: false,
-                livereload: true
-            },
-            //observar de stylus
-            stylesheets: {
-                files: ['stylus/*.styl'],
-                tasks: ['stylus']
-            },
-            //observar de js
-            scripts: {
-                files: ['javascript/*.js'],
-                tasks: ['uglify']
-            },
-            //observar el jade
-            jadephp: {
-                files: ["jade/*.jade"],
-                tasks: ["jadephp"]
-            }
+  grunt.initConfig({
+    //Preprocesador css
+    stylus: {
+      compile: {
+        options: {
+          compress: true,
+          linenos: false
         },
-        //Preprocesador css
-        stylus: {
-            compile: {
-                options: {
-                    compress: true,
-                    linenos: false
-                },
-                files: [{
-                    //sitio
-                    'www/css/index.css': 'stylus/estilos.styl',
-                }]
-            }
-        },
-        // Compresor de .js
-        uglify: {
-            options: {
-                mangle: false,
-                compress: {
-                    drop_console: true
-                }
-            },
-            js: {
-                files: {
-                    'www/js/index.js': 'javascript/*.js',
-                }
-            }
-        },
-        // Compilar Jade
-        jadephp: {
-            compile: {
-                options: {
-                    client: false,
-                    pretty: true
-                },
-                files: {
-                    'www/index.html': 'jade/index.jade',
-                }
-            }
-        },
-        //Notificaciones
-        notify: {
-            uglify: {
-                options: {
-                    enabled: true,
-                    max_jshint_notifications: 1,
-                    message: "uglify iniciado!"
-                }
-            },
-            jadephp: {
-                options: {
-                    enabled: true,
-                    max_jshint_notifications: 1,
-                    message: "jade iniciado!"
-                }
-            },
-            stylus: {
-                options: {
-                    enabled: true,
-                    max_jshint_notifications: 1,
-                    message: "stylus iniciado!"
-                }
-            },
-        },
-        //Server PHP
-        php: {
-            dist: {
-                options: {
-                    hostname: 'localhost',
-                    port: 8000,
-                    base: 'www', // Project root
-                    keepalive: false,
-                    open: false,
-                    directives: {
-                        'error_log': require('path').resolve('logs/error.log')
-                    }
-                }
-            }
-        },
-        //BrowserSync
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src: [
-                        'stylus/*.styl',
-                        'jade/*.jade',
-                        'javascript/*.js',
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    notify: false,
-                    open: true,
-                    server: {
-                        baseDir: "www"
-                    },
-                    ghostMode: {
-                        clicks: true,
-                        scroll: true,
-                        links: true,
-                        forms: true
-                    }
-                }
-            }
+        files: [{
+          //sitio
+          'www/css/index.css': 'stylus/estilos.styl',
+        }]
+      }
+    },
+    // Compresor de .js
+    uglify: {
+      options: {
+        mangle: false,
+        compress: {
+            drop_console: true
         }
-    });
+      },
+      js: {
+        files: {
+          'www/js/index.js': 'javascript/*.js',
+        }
+      }
+    },
+    // Compilar Jade
+    jadephp: {
+      compile: {
+        options: {
+          client: false,
+          pretty: true,
+          basedir: 'jade',
+          filename: 'base'
+        },
+        files: {
+          'www/index.html': 'jade/index.jade',
+          'www/blog.html': 'jade/blog.jade',
+          'www/blog_articulo.html': 'jade/blog_articulo.jade',
+          'www/cv.html': 'jade/cv.jade',
+          'www/contacto.html': 'jade/contacto.jade',
+        }
+      }
+    },
+    //Observar cambios
+    watch: {
+      options: {
+        nospawn: false,
+        livereload: true
+      },
+      //observar de stylus
+      stylesheets: {
+        files: ['stylus/*.styl'],
+        tasks: ['stylus']
+      },
+      //observar de js
+      scripts: {
+        files: ['javascript/*.js'],
+        tasks: ['uglify']
+      },
+      //observar el jade
+      jadephp: {
+        files: ["jade/*.jade"],
+        tasks: ["jadephp"]
+      }
+    },
+    //Notificaciones
+    notify: {
+      uglify: {
+        options: {
+          enabled: true,
+          max_jshint_notifications: 1,
+          message: "uglify iniciado!"
+        }
+      },
+      jadephp: {
+        options: {
+          enabled: true,
+          max_jshint_notifications: 1,
+          message: "jade iniciado!"
+        }
+      },
+      stylus: {
+        options: {
+          enabled: true,
+          max_jshint_notifications: 1,
+          message: "stylus iniciado!"
+        }
+      }
+    },
+    //Server PHP
+    php: {
+      dist: {
+        options: {
+          hostname: 'localhost',
+          port: 8000,
+          base: 'www', // Project root
+          keepalive: false,
+          open: false,
+          directives: {
+            'error_log': require('path').resolve('logs/error.log')
+          }
+        }
+      }
+    },
+    //BrowserSync
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'stylus/*.styl',
+            'jade/*.jade',
+            'javascript/*.js',
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: './www',
+          notify: false,
+          //startPath: '',
+          //reloadDelay: 2000,
+          //injectChanges: true,
+          reloadOnRestart: true,
+          ghostMode: {
+            clicks: true,
+            scroll: true,
+            links: true,
+            forms: true
+          }
+        }
+      }
+    }
+  });
 
-    //Cargamos todos los tasks declarados en package.json
-    require('load-grunt-tasks')(grunt);
-    // Defino las tareas.
-    grunt.registerTask('default', ['stylus', 'uglify', 'jadephp', 'notify', 'browserSync', 'watch']);
-    //grunt.registerTask('default', ['stylus', 'uglify', 'jadephp', 'notify', 'php:dist', 'browserSync', 'watch']);
+  //Cargamos todos los tasks declarados en package.json
+  require('load-grunt-tasks')(grunt);
+  // Defino las tareas.
+  grunt.registerTask('default', ['stylus', 'uglify', 'jadephp', 'notify', 'browserSync', 'watch']);
+  //grunt.registerTask('default', ['stylus', 'uglify', 'jadephp', 'notify', 'php:dist', 'browserSync', 'watch']);
 };
--- javascript/main.js
+++ javascript/main.js
@@ -1,32 +1,39 @@
 $(document).ready(function(){ //cuando el DOM listo
 	$("#navicon").toggle(
-        function(e){ //Primer click
-            $(".menuprincipal").addClass("efecto1");
-        },
-        function(e){ //Segundo click
-            $(".menuprincipal").removeClass("efecto1");
-        }
-    );
-    $("#acceder").toggle(
-        function(e){ //Primer click
-            $("#login").addClass("efecto2");
-        },
-        function(e){ //Segundo click
-            $("#login").addClass("efecto3");
-            setTimeout(function(){
-                $("#login").removeClass("efecto2");
-                $("#login").removeClass("efecto3");
-            }, 500);
-        }
-    );
-    $("#slider1").responsiveSlides({
-        auto: true,
-        random: true,
-        pager: false,
-        nav: true,
-        speed: 500,
-        timeout: 8000,
-        pause: true,
-        namespace: "centered-btns"
-    });
+    function(e){ //Primer click
+      $(".menuprincipal").addClass("efecto1");
+    },
+    function(e){ //Segundo click
+      $(".menuprincipal").removeClass("efecto1");
+    }
+  );
+  $("#acceder").toggle(
+    function(e){ //Primer click
+      $("#login").addClass("efecto2");
+    },
+    function(e){ //Segundo click
+      $("#login").addClass("efecto3");
+      setTimeout(function(){
+        $("#login").removeClass("efecto2");
+        $("#login").removeClass("efecto3");
+      }, 500);
+    }
+  );
+  $("#slider1").responsiveSlides({
+    auto: true,
+    random: true,
+    pager: false,
+    nav: true,
+    speed: 500,
+    timeout: 8000,
+    pause: true,
+    namespace: "centered-btns"
+  });
 });
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
